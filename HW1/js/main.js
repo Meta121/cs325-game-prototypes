9fa4d85d020a647482d8773a7bdc5182be8551a3
Meta121 <cpham15@masonlive.gmu.edu>
2020-02-12 21:28:47 -0500

    test4

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -53,10 +53,13 @@ function create() {
     ground.body.immovable = true;
 
     //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
+    //var ledge = platforms.create(400, 400, 'ground'); //original code
+	var ledge = platforms.create(100, 100, 'ground');
     ledge.body.immovable = true;
 
-    ledge = platforms.create(-150, 250, 'ground');
+    ledge = platforms.create(-150, 250, 'ground'); //original code
+	//ledge = platforms.create(-150, 250, 'ground');
+	
     ledge.body.immovable = true;
 
     // The player and its settings
