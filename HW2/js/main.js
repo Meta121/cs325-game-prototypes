3f44070b44f2f36f69b093bfd52a456cea1ffdb7
Meta121 <cpham15@masonlive.gmu.edu>
2020-02-19 23:34:36 -0500

    02-19-20 H2 Test 20

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -202,8 +202,9 @@ function createAliens () {
     aliens.y = 50;
 
     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-
+    //var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true); //original
+	var tween = game.add.tween(aliens).to( { x: 1000 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+	
     //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
 }
