e4bf522a89db754a4bb4195dd74d29ca5e2601d8
Meta121 <cpham15@masonlive.gmu.edu>
2020-02-19 22:18:16 -0500

    02-19-20 H2 Test 10

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -215,7 +215,7 @@ function createHearts () {
     hearts.y = 50;
 
     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true); //original
+    //var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true); //original
 
     //  When the tween loops it calls descend
     //tween.onLoop.add(descend, this); //original
