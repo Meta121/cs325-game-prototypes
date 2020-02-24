eb601407dc3b138cdad49ba6e4ccbe8460b4ec97
Meta121 <cpham15@masonlive.gmu.edu>
2020-02-23 21:47:01 -0500

    02-23-20 Test H 6

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -91,34 +91,7 @@ function make_main_game_state( game )
 		this.bird, this.pipes, this.restartGame, null, this);
 		}
     
-    
-	
-	//Prof. given code
-	return { "preload": preload, "create": create, "update": update }; //Prof. given code
-}
-
-
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
-	//var game = new Phaser.Game(400, 490); //Original given
-    
-    game.state.add( "main", make_main_game_state( game ) );
-    
-    game.state.start( "main" );
-	
-	//---------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------
 	// Make the bird jump 
 //jump: function() { //original
 function jump() { //test
@@ -171,6 +144,34 @@ function addRowOfPipes() { //test
 	this.labelScore.text = this.score;  
 //},
 }
+	
+	//Prof. given code
+	return { "preload": preload, "create": create, "update": update }; //Prof. given code
+}
+
+
+window.onload = function() {
+    // You might want to start with a template that uses GameStates:
+    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
+    
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+    
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
+	//var game = new Phaser.Game(400, 490); //Original given
+    
+    game.state.add( "main", make_main_game_state( game ) );
+    
+    game.state.start( "main" );
+	
+	//---------------------------------------------------------------------------------
+	
 	
 };
 
