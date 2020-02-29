4564db88faf1561cccf047e5748826a91a3b40ec
Meta121 <cpham15@masonlive.gmu.edu>
2020-02-28 20:20:23 -0500

    02-28-20 Test H 1

--- GameTesting/js/main.js
+++ GameTesting/js/main.js
@@ -1,6 +1,94 @@
 "use strict";
 
+function make_main_game_state( game )
+{
+    function preload() {
+		
+		//game.load.spritesheet('dude', 'assets/dude.png', 32, 48); //original from Phaser 2 Tutorial
+		this.game.load.spritesheet('player', 'assets/dude.png', 32, 48);
+        this.game.load.tilemap('tilemap', 'assets/level.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/tutorial2_tm.png');
+		
+
+    }
+    
+    var bouncy;
+    
+    function create() {
+		
+		//  Our two animations, walking left and right.
+		//player.animations.add('left', [0, 1, 2, 3], 10, true); //original
+		// player.animations.add('right', [5, 6, 7, 8], 10, true); //original
+		
+		//-Tutorial Code--------------------------------------------------------------------------
+		//----Importing TileMap into Phaser-------------------------------------
+		//Start the Arcade Physics systems
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //Change the background colour
+        this.game.stage.backgroundColor = "#a9f0ff";
+
+        //Add the tilemap and tileset image. The first parameter in addTilesetImage
+        //is the name you gave the tilesheet when importing it into Tiled, the second
+        //is the key to the asset in Phaser
+        this.map = this.game.add.tilemap('tilemap');
+        //this.map.addTilesetImage('tiles128', 'tiles'); //original
+		this.map.addTilesetImage('tutorial2_tm', 'tiles'); //test
+		
+
+        //Add both the background and ground layers. We won't be doing anything with the
+        //GroundLayer though
+        this.backgroundlayer = this.map.createLayer('BackgroundLayer');
+        this.groundLayer = this.map.createLayer('GroundLayer');
+
+        //Before you can use the collide function you need to set what tiles can collide
+        this.map.setCollisionBetween(1, 100, true, 'GroundLayer');
+
+        //Change the world size to match the size of this layer
+        this.groundLayer.resizeWorld();
+		
+		//----Creating a Player----------------------------------
+		//Set some physics on the sprite
+        this.sprite.body.bounce.y = 0.2;
+        this.sprite.body.gravity.y = 2000;
+        this.sprite.body.gravity.x = 20;
+        this.sprite.body.velocity.x = 100;
+
+        //Create a running animation for the sprite and play it
+        //this.sprite.animations.add('right', [5, 6, 7, 8], 10, true); //original
+        //this.sprite.animations.play('right'); //original
+		
+		this.sprite.animations.add('left', [0, 1, 2, 3], 10, true); //test
+		this.sprite.animations.add('right', [5, 6, 7, 8], 10, true); //test
+        this.sprite.animations.play('right'); //original
+
+        //Make the camera follow the sprite //Sets the camera to follow the player
+        this.game.camera.follow(this.sprite); //original
+
+        //Enable cursor keys so we can create some controls
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+		
+    }
+    
+    function update() {
+		
+		//Make the sprite collide with the ground layer
+        this.game.physics.arcade.collide(this.sprite, this.groundLayer);
+		
+		//Make the sprite jump when the up key is pushed
+        if(this.cursors.up.isDown) {
+          this.sprite.body.velocity.y = -500;
+        }
+    }
+    
+    return { "preload": preload, "create": create, "update": update };
+}
+
+
 window.onload = function() {
+    // You might want to start with a template that uses GameStates:
+    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
+    
     // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
     // You will need to change the fourth parameter to "new Phaser.Game()" from
     // 'phaser-example' to 'game', which is the id of the HTML element where we
@@ -10,121 +98,12 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
     
-    function preload() {
-        //Prof. default given code
-		/*
-		// Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-		*/
-		
-		//-------------------------------------------------------------------------
-		// Load the bird sprite
-		game.load.image('bird', 'assets/bird.png'); 
-		game.load.image('pipe', 'assets/pipe.png');
-    }
+    game.state.add( "main", make_main_game_state( game ) );
     
-    var bouncy;
-    
-    function create() {
-        //----------------------------------------------------------------------------------------------
-		// Change the background color of the game to blue
-		game.stage.backgroundColor = '#71c5cf';
-
-		// Set the physics system
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-
-		// Display the bird at the position x=100 and y=245
-		this.bird = game.add.sprite(100, 245, 'bird');
-
-		// Add physics to the bird
-		// Needed for: movements, gravity, collisions, etc.
-		game.physics.arcade.enable(this.bird);
-
-		// Add gravity to the bird to make it fall
-		this.bird.body.gravity.y = 1000;  
-
-		// Call the 'jump' function when the spacekey is hit
-		var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-		//spaceKey.onDown.add(this.jump, this); //original
-		spaceKey.onDown.add(jump); //test
-		
-		
-		
-		// Create an empty group
-		this.pipes = game.add.group(); 
-		
-		this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
-		
-		this.score = 0;
-		this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });  
-    }
-    
-    function update() {
-        //--------------------------------------------------------------------------------------------------
-		// If the bird is out of the screen (too high or too low)
-		// Call the 'restartGame' function
-		if (this.bird.y < 0 || this.bird.y > 490) {
-			//this.restartGame(); //original
-			restartGame();
-		}
-		
-		game.physics.arcade.overlap(
-		this.bird, this.pipes, this.restartGame, null, this);
-    }
+    game.state.start( "main" );
 	
-	//------------------------------------------------------------------------------------
-	// Make the bird jump 
-	//jump: function() { //original
-	function jump() { //test
-		// Add a vertical velocity to the bird
-		this.bird.body.velocity.y = -350;
-	//}, original
-	} //test
-
-	// Restart the game
-	//restartGame: function() { //original
-	function restartGame() { //test
-		// Start the 'main' state, which restarts the game
-		game.state.start('main');
-	//},
-	}
-
-	//addOnePipe: function(x, y) { //original
-	function addOnePipe(x, y) { //test
-		// Create a pipe at the position x and y
-		var pipe = game.add.sprite(x, y, 'pipe');
-
-		// Add the pipe to our previously created group
-		this.pipes.add(pipe);
-
-		// Enable physics on the pipe 
-		game.physics.arcade.enable(pipe);
-
-		// Add velocity to the pipe to make it move left
-		pipe.body.velocity.x = -200; 
-
-		// Automatically kill the pipe when it's no longer visible 
-		pipe.checkWorldBounds = true;
-		pipe.outOfBoundsKill = true;
-	//},
-	}
-
-//addRowOfPipes: function() { //original
-	function addRowOfPipes() { //test
-		// Randomly pick a number between 1 and 5
-		// This will be the hole position
-		var hole = Math.floor(Math.random() * 5) + 1;
-
-			// Add the 6 pipes 
-		// With one big hole at position 'hole' and 'hole + 1'
-		for (var i = 0; i < 8; i++)
-			if (i != hole && i != hole + 1) 
-				this.addOnePipe(400, i * 60 + 10);   
-		
-		this.score += 1;
-		this.labelScore.text = this.score;  
-	//},
-	}
+	//--------------------------------
+	
 };
